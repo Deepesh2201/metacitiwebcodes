91d86107b31e4e44e87b03ae5c7735c6caf50662
Tricker-khan <mohdsuhail78676@gmail.com>
2023-02-20 01:28:13 +0530

    bid id key added

--- app/Models/Request/TripBids.php
+++ app/Models/Request/TripBids.php
@@ -4,8 +4,38 @@ namespace App\Models\Request;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class TripBids extends Model
 {
     use HasFactory;
+
+    public function request(){
+        return $this->belongsTo(Request::class, 'request_id', 'id');
+    }
+
+    /**
+    * Get formated and converted timezone of user's created at.
+    * @return string
+    */
+    public function getConvertedCreatedAtAttribute()
+    {
+        if ($this->created_at==null) {
+            return null;
+        }
+        $timezone = $this->request->serviceLocationDetail?->timezone?:env('SYSTEM_DEFAULT_TIMEZONE');
+        return Carbon::parse($this->created_at)->setTimezone($timezone)->format('jS M h:i A');
+    }
+    /**
+    * Get formated and converted timezone of user's created at.
+    * @return string
+    */
+    public function getConvertedUpdatedAtAttribute()
+    {
+        if ($this->updated_at==null) {
+            return null;
+        }
+        $timezone = $this->request->serviceLocationDetail?->timezone?:env('SYSTEM_DEFAULT_TIMEZONE');
+        return Carbon::parse($this->updated_at)->setTimezone($timezone)->format('jS M h:i A');
+    }
 }
